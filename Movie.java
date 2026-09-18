f3c8b3c6014afd374285a584d66f7829a2c24f80
mavy-101 <mavecabahug2@gmail.com>
2026-09-17 17:56:28 -0700

    Delete Movie.java

--- Movie.java
+++
@@ -1,9 +0,0 @@
-public class Movie {
-    String title;
-    String genre;
-    int duration;
- 
-    void displayInfo() {
-        System.out.println(title + " (" + genre + ") - " + duration + " minutes");
-    }
-}
